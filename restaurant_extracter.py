9199092fb1de63c093655c1450bf6066836cbe0e
sangwon1000 <81509635+sangwon1000@users.noreply.github.com>
2024-10-16 08:20:45 +0800

    refactor recipe extractor into separate class

---
+++ restaurant_extracter.py
@@ -0,0 +1,10 @@
+"""
+https://www.youtube.com/watch?v=wu1fOmsPEr8&list=PLuMuHAJh9g_Py_PSm8gmHdlcil6CQ9QCM&index=154
+
+this video is review of restaurant(s)
+
+both in korean and englsih
+what restaurant is covered?
+what dishes are covered?
+what is being said about each dish covered?
+"""
